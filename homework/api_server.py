65a15d8f37cc62d91018c7dfab17ebec85b62f5d
jdlodonog <101781006+jdlodonog@users.noreply.github.com>
2024-11-16 08:46:02 -0500

    a

--- homework/api_server.py
+++ homework/api_server.py
@@ -9,6 +9,8 @@
 # "sqft_lot": "2200", "floors": "1", "waterfront": "1", "condition": "3"}'
 #
 
+#curl http://127.0.0.1:5000 -X POST -H "Content-Type: application/json" -d "{\"bathrooms\": \"2\", \"bedrooms\": \"3\", \"sqft_living\": \"1800\", \"sqft_lot\": \"2200\", \"floors\": \"1\", \"waterfront\": \"1\", \"condition\": \"3\"}"
+
 import pickle
 
 import pandas as pd  # type: ignore
